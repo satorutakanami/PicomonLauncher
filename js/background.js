70defceb6b17889368667ad2e47d086e65378737
Satoru Takanami <satorutakanami@gmail.com>
2013-07-12 13:18:35 +0900

    initial

---
+++ js/background.js
@@ -0,0 +1,19 @@
+/*var clientId = '';
+var profileId = '';
+var pageUri = '';
+
+var fire = function() {
+	var _pcmnq = _pcmnq || [];
+	_pcmnq.push(['account', 'PA-000001-1']);
+	(function(){
+	var pc=document.createElement('script');pc.type='text/javascript';pc.async=true;
+	pc.src=('https:' == document.location.protocol?'https://battamon.net:3011':'http://battamon.net:3010')+'/loader.js';
+	var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(pc,s);
+	})();
+};
+*/
+/*
+chrome.browserAction.onClicked.addListener(function(tab) {
+	tab.alert(3);
+	chrome.tabs.executeScript(null, {file: "js/contentsScript.js"});
+});*/
---
+++ js/contentsScript.js
@@ -0,0 +1,7 @@
+var _pcmnq = _pcmnq || [];
+_pcmnq.push(['account', 'PA-000001-1']);
+(function(){
+var pc=document.createElement('script');pc.type='text/javascript';pc.async=true;
+pc.src=('https:' == document.location.protocol?'https://battamon.net:3011':'http://battamon.net:3010')+'/loader.js';
+var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(pc,s);
+})();
